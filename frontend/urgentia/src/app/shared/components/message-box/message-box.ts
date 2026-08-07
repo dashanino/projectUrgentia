fb6c59dbcd814cbed0489fdf662f05b9f5ec56f8
dashanino <nino.dasha@uces.edu.co>
2026-08-07 14:19:50 -0500

    Terminando de agregar estructura inicial de frontend

---
+++ frontend/urgentia/src/app/shared/components/message-box/message-box.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-box',
+  imports: [],
+  templateUrl: './message-box.html',
+  styleUrl: './message-box.scss',
+})
+export class MessageBox {}
