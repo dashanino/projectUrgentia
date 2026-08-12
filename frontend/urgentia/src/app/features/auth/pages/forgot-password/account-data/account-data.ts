1c092bd6a66e0e5bdbcbdd5fd8de6a0dd10eaf5b
dashanino <nino.dasha@uces.edu.co>
2026-08-12 13:52:49 -0500

    forgot-password components agregados

---
+++ frontend/urgentia/src/app/features/auth/pages/forgot-password/account-data/account-data.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-data',
+  imports: [],
+  templateUrl: './account-data.html',
+  styleUrl: './account-data.scss',
+})
+export class AccountData {}
