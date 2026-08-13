51d3ebd9cc792cdbbb68f93078b90fa5c52e7eb9
dashanino <nino.dasha@uces.edu.co>
2026-08-12 22:14:41 -0500

    corrección de verify-code

--- frontend/urgentia/src/app/features/auth/pages/forgot-password/verify-code/verify-code.ts
+++ frontend/urgentia/src/app/features/auth/pages/forgot-password/verify-code/verify-code.ts
@@ -25,7 +25,7 @@ export class VerifyCode {
   submit() {
   if (this.form.valid) {
     console.log('Form submitted', this.form.value);
-    this.router.navigate(['/recuperar-contraseña/success']);
+    this.router.navigate(['/recuperar-contraseña/exito']);
   }
 }
 }
