915bdfdb9453454255853f283bf463d8970c75ed
Aslybri <amado.asly@uces.edu.co>
2026-08-23 16:50:57 -0500

    agg banderas rojas embarazada

--- frontend/urgentia/src/app/features/emergency/pages/red-flags/embarazada/embarazada.ts
+++ frontend/urgentia/src/app/features/emergency/pages/red-flags/embarazada/embarazada.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-embarazada',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './embarazada.html',
   styleUrl: './embarazada.scss',
 })
-export class Embarazada {}
+export class Embarazada {
+  private router = inject(Router);
+  banderaSeleccionada: string | null = null;
+
+  seleccionarBandera(bandera: string) {
+    this.banderaSeleccionada = bandera;
+  }
+
+  estaSeleccionada(bandera: string): boolean {
+    return this.banderaSeleccionada === bandera;
+  }
+  goBack() {
+    this.router.navigate(['/patient-group']);
+  }
+}
