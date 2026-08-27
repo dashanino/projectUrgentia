af7c9ea738c6c6b0146c160d5cc13e31128d0c16
dashanino <nino.dasha@uces.edu.co>
2026-08-27 00:54:55 -0500

    base de TAREA completa

--- frontend/urgentia/src/app/app.routes.ts
+++ frontend/urgentia/src/app/app.routes.ts
@@ -91,6 +91,20 @@ export const routes: Routes = [
             (m) => m.Perfil
           ),
       },
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('./pages/dashboard/dashboard').then(
+            (m) => m.Dashboard
+          ),
+      },
+      {
+        path: 'usuarios',
+        loadComponent: () =>
+          import('./pages/usuarios/usuarios').then(
+            (m) => m.Usuarios
+          ),
+      },
 
     ],
   },
--- frontend/urgentia/src/app/components/header/header.ts
+++ frontend/urgentia/src/app/components/header/header.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [MatIconModule],
+  imports: [MatIconModule,RouterLink],
   standalone:true,
   templateUrl: './header.html',
   styleUrl: './header.scss',
