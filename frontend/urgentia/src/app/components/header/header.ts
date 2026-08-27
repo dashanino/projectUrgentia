c7d80282a73a3e119e94fc1b963e3bcee8a0746d
dashanino <nino.dasha@uces.edu.co>
2026-08-27 00:23:29 -0500

    pequeños cambios

--- frontend/urgentia/src/app/components/header/header.ts
+++ frontend/urgentia/src/app/components/header/header.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatIconModule],
+  standalone:true,
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
+
 export class Header {}
--- frontend/urgentia/src/app/layouts/main-layout/main-layout.ts
+++ frontend/urgentia/src/app/layouts/main-layout/main-layout.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
 
 import { Header } from '../../components/header/header';
 import { Sidebar } from '../../components/sidebar/sidebar';
 import { Footer } from '../../components/footer/footer';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [],
+  imports: [Header,Sidebar,Footer,RouterOutlet],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
